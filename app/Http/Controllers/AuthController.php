549eb78ec5c111b0aa39f3e5676a9724f64b23ba
tosinibrahim96 <tosinibrahim96@gmail.com>
2022-02-05 13:21:58 +0100

    Add form validation class

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateNewUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -11,29 +12,11 @@ class AuthController extends Controller
 {
   /**
    * create a new account
-   * @param \Illuminate\Http\Request $request
+   * @param App\Http\Requests\CreateNewUserRequest $request
    * @return \Illuminate\Http\Response
    */
-  public function register(Request $request)
+  public function register(CreateNewUserRequest $request)
   {
-    /*--------Concern 1(Request validation)------------------*/
-    $validator = Validator::make($request->all(), [
-      'email' => 'required|email|unique:users',
-      'name' => 'required|string',
-      'password' => 'required|min:6'
-    ]);
-
-    if ($validator->fails()) {
-      /*--------Concern 3(Response formatting and return)-----------------*/
-      return response()->json(
-        [
-          "status" => false,
-          "message" => $validator->errors()
-        ],
-        400
-      );
-    }
-
     /*--------Concern 2 (Business logic execution)------------------*/
     $password = Hash::make($request->password);
     $user = User::create([
